b61df3f4eda085cca6fe5f05890f3609eb3fe851
rdas-nev <restrela20@gmail.com>
2024-02-27 16:12:11 +0000

    step1: Data Analysis - The rules are stupid so fuck'em

---
+++ data_analysis/describe.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import sys
+
+
+def ft_describe(data):
+    numerical_cols = data.select_dtypes(include=[np.number]).columns
+    result = pd.DataFrame(index=['Count', 'Mean', 'Std', 'Min', '25%', '50%', '75%', 'Max'])
+
+    for column in numerical_cols:
+        data[column] = data[column].fillna(data[column].mean())
+        result[column] = [
+            len(data[column]),
+            data[column].mean(),
+            data[column].std(),
+            data[column].min(),
+            data[column].quantile(0.25),
+            data[column].median(),
+            data[column].quantile(0.75),
+            data[column].max()
+        ]
+    return result
+
+
+def main():
+    try:
+        # load dataset
+        data = pd.read_csv(sys.argv[1])
+        # print ft_describe
+        print(ft_describe(data))
+
+    except FileNotFoundError:
+        print("Error: Invalid file.")
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
