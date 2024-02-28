3eefba1d17b91c5dac962d212919d7da9f233d16
rdas-nev <restrela20@gmail.com>
2024-02-28 15:40:01 +0000

    step 2.1 - done

--- data_visualization/histogram.py
+++ data_visualization/histogram.py
@@ -1,26 +1,31 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 
 
 def histogram(data, course_cols, house_cols):
     num_features = len(course_cols)
     num_categories = len(data[house_cols].unique())
+    categories = data[house_cols].unique()
 
-    fig, axes = plt.subplots(nrows=num_features, ncols=1, figsize=(15, 10 * num_features))
 
+    fig, axes = plt.subplots(nrows=num_features, ncols=1, figsize=(8, 3 * num_features))
+
+    counter = 0
     for i, feature_column in enumerate(course_cols):
         ax = axes[i]
-        for category_value in data[house_cols].unique():
+        for category_value in categories:
             subset = data[data[house_cols] == category_value]
-            ax.hist(subset[feature_column], bins=20, alpha=0.5, label=f'Category {category_value}')
+            ax.hist(subset[feature_column], bins=20, alpha=0.5, label=f'House {category_value}')
 
-        # ax.set_title(f'{feature_column}')
         ax.set_xlabel(feature_column)
-        ax.set_ylabel('score')
-        # ax.legend()
-
-    # plt.tight_layout()
+        ax.set_xticks([])
+        if counter == 0:
+            ax.legend()
+            counter += 1
+        
+    plt.subplots_adjust(hspace=0.5)
     plt.show()
 
 
